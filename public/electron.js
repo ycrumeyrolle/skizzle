611f9639b0c5642f4338e11028d33dafbf6eb687
Jérôme Boukorras <jerome.boukorras@axa.fr>
2020-05-27 10:09:09 +0200

    fix: Tray icon for macOS

--- public/electron.js
+++ public/electron.js
@@ -1,23 +1,36 @@
-require('electron-reload')(__dirname);
-const electron = require('electron');
+require("electron-reload")(__dirname);
+const os = require("os");
+const electron = require("electron");
 const { app, BrowserWindow, Menu, Notification, ipcMain, Tray } = electron;
 
+const platforms = {
+  WINDOWS: "WINDOWS",
+  MAC: "MAC"
+};
+
+const platformsNames = {
+  win32: platforms.WINDOWS,
+  darwin: platforms.MAC
+};
+
+const currentPlatform = platformsNames[os.platform()];
+
 const config = {
-  clientId: '26940866-2575-4627-B2A4-DFF5972172B3',
-  clientSecret: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIs',
+  clientId: "26940866-2575-4627-B2A4-DFF5972172B3",
+  clientSecret: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIs",
   clientAssertion:
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Im9PdmN6NU1fN3AtSGpJS2xGWHo5M3VfVjBabyJ9.eyJjaWQiOiIyNjk0MDg2Ni0yNTc1LTQ2MjctYjJhNC1kZmY1OTcyMTcyYjMiLCJjc2kiOiJlMmMyNmQyOC00MDljLTQ2OTAtYWM4Mi1mZDc5Y2U3NDk0NjgiLCJuYW1laWQiOiI2ZTBkMzcwYi01ZDA1LTY3ODgtYjk0ZC1lY2E2ODU5ZTRhZTEiLCJpc3MiOiJhcHAudnN0b2tlbi52aXN1YWxzdHVkaW8uY29tIiwiYXVkIjoiYXBwLnZzdG9rZW4udmlzdWFsc3R1ZGlvLmNvbSIsIm5iZiI6MTU3OTg4MjU4OSwiZXhwIjoxNzM3NzM1Mzg5fQ.JZ3XeXL22-nw-9BHi3sigm_Wruj1uPKBO-bA_um3tFaauex2eTvsEPPJZ3C5GYOdldroMRE_UGZUvBNctL2Ya6JjjESWEwwhTb2kkHs9r466ewnU7l-UfjdWV-cPJoKWlfEhU7IfH1PD1eSPJijXeB6zQYpPkM-TNAyVZl3PwOoOFdqkQrV1_eufxfiYeO9aBaxCCtzo_b3PsgvSVuZxGVWVdS0svX8bB_RKiwcWCcR089-5OnhK38OZVfx-RVP2HF2Eb-xmqTsQjcvWMdBam_sND3HKeo02GnxrByBizvTLyb6cE1yJJ-K1YY9vIGuWMaYjBLXFWEykYD60tr_bog',
-  authorizationUrl: 'https://app.vssps.visualstudio.com/oauth2/authorize',
-  tokenUrl: 'https://app.vssps.visualstudio.com/oauth2/token',
-  logout: 'https://app.vsaex.visualstudio.com/_signout',
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Im9PdmN6NU1fN3AtSGpJS2xGWHo5M3VfVjBabyJ9.eyJjaWQiOiIyNjk0MDg2Ni0yNTc1LTQ2MjctYjJhNC1kZmY1OTcyMTcyYjMiLCJjc2kiOiJlMmMyNmQyOC00MDljLTQ2OTAtYWM4Mi1mZDc5Y2U3NDk0NjgiLCJuYW1laWQiOiI2ZTBkMzcwYi01ZDA1LTY3ODgtYjk0ZC1lY2E2ODU5ZTRhZTEiLCJpc3MiOiJhcHAudnN0b2tlbi52aXN1YWxzdHVkaW8uY29tIiwiYXVkIjoiYXBwLnZzdG9rZW4udmlzdWFsc3R1ZGlvLmNvbSIsIm5iZiI6MTU3OTg4MjU4OSwiZXhwIjoxNzM3NzM1Mzg5fQ.JZ3XeXL22-nw-9BHi3sigm_Wruj1uPKBO-bA_um3tFaauex2eTvsEPPJZ3C5GYOdldroMRE_UGZUvBNctL2Ya6JjjESWEwwhTb2kkHs9r466ewnU7l-UfjdWV-cPJoKWlfEhU7IfH1PD1eSPJijXeB6zQYpPkM-TNAyVZl3PwOoOFdqkQrV1_eufxfiYeO9aBaxCCtzo_b3PsgvSVuZxGVWVdS0svX8bB_RKiwcWCcR089-5OnhK38OZVfx-RVP2HF2Eb-xmqTsQjcvWMdBam_sND3HKeo02GnxrByBizvTLyb6cE1yJJ-K1YY9vIGuWMaYjBLXFWEykYD60tr_bog",
+  authorizationUrl: "https://app.vssps.visualstudio.com/oauth2/authorize",
+  tokenUrl: "https://app.vssps.visualstudio.com/oauth2/token",
+  logout: "https://app.vsaex.visualstudio.com/_signout",
   useBasicAuthorizationHeader: false,
-  redirectUri: 'https://localhost:3000/',
+  redirectUri: "https://localhost:3000/",
   scope:
-    'vso.analytics vso.build vso.code vso.connected_server vso.dashboards vso.entitlements vso.extension vso.extension.data vso.graph vso.identity vso.loadtest vso.machinegroup_manage vso.memberentitlementmanagement vso.notification vso.packaging vso.project vso.release vso.securefiles_read vso.serviceendpoint vso.symbols vso.taskgroups_read vso.test vso.variablegroups_read vso.wiki vso.work',
+    "vso.analytics vso.build vso.code vso.connected_server vso.dashboards vso.entitlements vso.extension vso.extension.data vso.graph vso.identity vso.loadtest vso.machinegroup_manage vso.memberentitlementmanagement vso.notification vso.packaging vso.project vso.release vso.securefiles_read vso.serviceendpoint vso.symbols vso.taskgroups_read vso.test vso.variablegroups_read vso.wiki vso.work"
 };
 function setAppUserModelId() {
-  global.appUserModelId = 'skizzle';
-  app.setAppUserModelId('skizzle');
+  global.appUserModelId = "skizzle";
+  app.setAppUserModelId("skizzle");
 }
 
 setAppUserModelId();
@@ -34,7 +47,7 @@ app.whenReady().then(() => {
 
 function createWindow() {
   window = new BrowserWindow({
-    title: 'Skizzle',
+    title: "Skizzle",
     center: true,
     width: 1024,
     height: 768,
@@ -43,80 +56,82 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: true,
       experimentalFeatures: true,
-      webviewTag: true,
-    },
+      webviewTag: true
+    }
   });
 
-  window.loadURL('file:///' + __dirname + '/index.html');
+  window.loadURL("file:///" + __dirname + "/index.html");
 
-  window.on('closed', () => {
+  window.on("closed", () => {
     window = null;
   });
 
-  window.once('focus', () => window.flashFrame(false));
+  window.once("focus", () => window.flashFrame(false));
   window.flashFrame(true);
 
-  const iconName = '/assets/icon.png';
+  const iconName =
+    currentPlatform === "MAC" ? "/assets/icon-macos.png" : "/assets/icon.png";
+
   const iconPath = __dirname + iconName;
   tray = new Tray(iconPath);
-  tray.setToolTip('Skizzle application');
+  tray.setToolTip("Skizzle application");
 
   const contextMenu = Menu.buildFromTemplate([
     {
-      label: 'Application',
+      label: "Application",
       submenu: [
         {
-          label: 'About Application',
-          selector: 'orderFrontStandardAboutPanel:',
+          label: "About Application",
+          selector: "orderFrontStandardAboutPanel:"
         },
-        { type: 'separator' },
+        { type: "separator" },
         {
-          label: 'Quit',
-          accelerator: 'Command+Q',
+          label: "Quit",
+          accelerator: "Command+Q",
           click: function() {
             app.quit();
-          },
-        },
-      ],
+          }
+        }
+      ]
     },
     {
-      label: 'Edit',
+      label: "Edit",
       submenu: [
-        { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
-        { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
-        { type: 'separator' },
-        { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
-        { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
-        { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
         {
-          label: 'Select All',
-          accelerator: 'CmdOrCtrl+A',
-          selector: 'selectAll:',
-        },
-      ],
+          label: "Select All",
+          accelerator: "CmdOrCtrl+A",
+          selector: "selectAll:"
+        }
+      ]
     },
     {
-      label: 'Ouvrir la console de développement',
-      click: () => window.webContents.openDevTools({ mode: 'detach' }),
-      accelerator: 'CommandOrControl+O',
+      label: "Ouvrir la console de développement",
+      click: () => window.webContents.openDevTools({ mode: "detach" }),
+      accelerator: "CommandOrControl+O"
     },
     {
       label: "Recharger l'application",
       click: () => window.reload(),
-      accelerator: 'F5',
+      accelerator: "F5"
     },
-    { type: 'separator' },
+    { type: "separator" },
     {
-      label: 'Quitter',
+      label: "Quitter",
       click: () => app.quit(),
-      accelerator: 'CommandOrControl+Q',
-    },
+      accelerator: "CommandOrControl+Q"
+    }
   ]);
 
   tray.setContextMenu(contextMenu);
   Menu.setApplicationMenu(contextMenu);
 
-  tray.on('click', () => {
+  tray.on("click", () => {
     if (window.isMinimized()) window.restore();
     if (!window.isVisible()) window.show();
     window.focus();
@@ -127,12 +142,12 @@ if (app.isPackaged) {
   const settings = app.getLoginItemSettings();
 
   app.setLoginItemSettings({
-    openAtLogin: settings.openAtLogin,
+    openAtLogin: settings.openAtLogin
   });
 
-  ipcMain.on('launch-startup', (event, arg) => {
+  ipcMain.on("launch-startup", (event, arg) => {
     app.setLoginItemSettings({
-      openAtLogin: arg,
+      openAtLogin: arg
     });
   });
 }
@@ -142,7 +157,7 @@ const gotTheLock = app.requestSingleInstanceLock();
 if (!gotTheLock) {
   app.quit();
 } else {
-  app.on('second-instance', (event, commandLine, workingDirectory) => {
+  app.on("second-instance", (event, commandLine, workingDirectory) => {
     if (window) {
       if (window.isMinimized()) window.restore();
       if (!window.isVisible()) window.show();
@@ -150,21 +165,21 @@ if (!gotTheLock) {
     }
   });
 
-  app.commandLine.appendSwitch('no-proxy-server');
-  app.commandLine.appendSwitch('disable-site-isolation-trials');
-  app.on('ready', createWindow);
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
+  app.commandLine.appendSwitch("no-proxy-server");
+  app.commandLine.appendSwitch("disable-site-isolation-trials");
+  app.on("ready", createWindow);
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
       app.quit();
     }
   });
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (window === null) {
       createWindow();
     }
   });
 
-  ipcMain.on('notifier', (event, arg) => {
+  ipcMain.on("notifier", (event, arg) => {
     const { body, title } = arg;
 
     if (Notification.isSupported()) {
@@ -172,7 +187,7 @@ if (!gotTheLock) {
     }
   });
 
-  ipcMain.on('azure-devops-oauth', (event, arg) => {
+  ipcMain.on("azure-devops-oauth", (event, arg) => {
     let authWindow = new BrowserWindow({
       show: false,
       modal: true,
@@ -180,33 +195,33 @@ if (!gotTheLock) {
       parent: window,
       webPreferences: {
         nodeIntegration: false,
-        contextIsolation: true,
-      },
+        contextIsolation: true
+      }
     });
 
     authWindow.show();
 
     authWindow.webContents.loadURL(
-      `${config.authorizationUrl}?client_id=${config.clientId}&client_secret=${config.clientSecret}&response_type=code&redirect_uri=${config.redirectUri}&response_mode=query&scope=${config.scope}`,
+      `${config.authorizationUrl}?client_id=${config.clientId}&client_secret=${config.clientSecret}&response_type=code&redirect_uri=${config.redirectUri}&response_mode=query&scope=${config.scope}`
     );
 
-    authWindow.on('closed', () => {
+    authWindow.on("closed", () => {
       authWindow = null;
     });
-    authWindow.webContents.on('will-redirect', (e, url) => {
+    authWindow.webContents.on("will-redirect", (e, url) => {
       const details = url;
 
       if (details && details.startsWith(config.redirectUri)) {
-        const _url = details.split('?')[1];
+        const _url = details.split("?")[1];
         const _params = new URLSearchParams(_url);
-        const _accessCode = _params.get('code');
+        const _accessCode = _params.get("code");
 
         if (_accessCode) {
-          event.sender.send('getToken', {
+          event.sender.send("getToken", {
             url: config.tokenUrl,
             redirect_uri: config.redirectUri,
             client_assertion: config.clientAssertion,
-            access_code: _accessCode,
+            access_code: _accessCode
           });
 
           authWindow.close();
@@ -215,21 +230,21 @@ if (!gotTheLock) {
     });
   });
 
-  ipcMain.on('logout', () => {
+  ipcMain.on("logout", () => {
     let logoutWindow = new BrowserWindow({
       show: false,
       modal: true,
       autoHideMenuBar: true,
-      parent: window,
+      parent: window
     });
 
     logoutWindow.show();
 
     logoutWindow.webContents.loadURL(
-      `${config.logout}?redirect_uri=${config.redirectUri}`,
+      `${config.logout}?redirect_uri=${config.redirectUri}`
     );
 
-    logoutWindow.on('closed', () => {
+    logoutWindow.on("closed", () => {
       logoutWindow = null;
     });
   });
